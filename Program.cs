951f0843ee9299809160b776eb5b25113c3db8d5
jotaro4u <kothaker135@gmail.com>
2024-12-10 00:27:49 +0300

    gdf

--- Program.cs
+++ Program.cs
@@ -8,24 +8,55 @@ namespace ConsoleApp_n11p
 {
     class Program
     {
-        static void CalculateCircle(double r)
+        static double Input(string sideName)
         {
-            double s = 3.14 * r * r;
-            double cs = 2 * 3.14 * r;
-            Console.WriteLine($"Радиус: {r}");
-            Console.WriteLine($"Площадь круга: {s}");
-            Console.WriteLine($"Длина окружности: {cs}");
+            Console.Write($"{sideName}: ");
+            return double.Parse(Console.ReadLine());
+        }
+
+        static double P(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        static double S(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+        }
+
+        static void Sum(double a1, double b1, double c1, double a2, double b2, double c2)
+        {
+            double p1 = P(a1, b1, c1);
+            double s1 = S(a1, b1, c1);
+            double p2 = P(a2, b2, c2);
+            double s2 = S(a2, b2, c2);
+
+            double totalP = p1 + p2;
+            double totalS = s1 + s2;
+
+            Console.WriteLine($"Сумма периметров: {totalP}");
+            Console.WriteLine($"Сумма площадей: {totalS}");
         }
         /// <summary>
-        /// Задача 4. Создать метод, который будет вычислять площадь круга и длину окружности.
-        /// Вызывающий программный код должен передавать в качестве аргумента методу значение радиуса и печатать значение радиуса,
-        /// площади круга и длины окружности.
+        /// Задача 5. Даны стороны двух треугольников. Найти сумму их периметров и сумму их площадей.
+        /// (Определить вспомогательный метод для расчета периметра и площади треугольника по его сторонам.)
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            double r = 5;
-            CalculateCircle(r);
+            Console.WriteLine("Введите стороны первого треугольника:");
+            double a1 = Input("Сторона a");
+            double b1 = Input("Сторона b");
+            double c1 = Input("Сторона c");
+
+            Console.WriteLine("Введите стороны второго треугольника:");
+            double a2 = Input("Сторона a");
+            double b2 = Input("Сторона b");
+            double c2 = Input("Сторона c");
+
+            Sum(a1, b1, c1, a2, b2, c2);
+
             Console.ReadLine();
         }
     }
