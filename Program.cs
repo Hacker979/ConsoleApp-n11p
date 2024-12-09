dd6175f83833b96a92395d0a8c961da4f18727ef
jotaro4u <kothaker135@gmail.com>
2024-12-10 00:43:21 +0300

    af

--- Program.cs
+++ Program.cs
@@ -13,34 +13,44 @@ namespace ConsoleApp_n11p
             Console.Write(msg);
             return double.Parse(Console.ReadLine());
         }
+
         static double Dist(double x1, double y1, double x2, double y2)
         {
             return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
 
-        static double P(double x1, double y1, double x2, double y2, double x3, double y3)
+        static double Area(double x1, double y1, double x2, double y2, double x3, double y3)
         {
-            double side1 = Dist(x1, y1, x2, y2);
-            double side2 = Dist(x2, y2, x3, y3);
-            double side3 = Dist(x3, y3, x1, y1);
-            return side1 + side2 + side3;
+            return Math.Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
+        }
+
+        static double P(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double x5, double y5)
+        {
+            double area1 = Area(x1, y1, x2, y2, x3, y3);
+            double area2 = Area(x1, y1, x3, y3, x4, y4);
+            double area3 = Area(x1, y1, x4, y4, x5, y5);
+            return area1 + area2 + area3;
         }
         /// <summary>
-        /// Задача 6. Найти периметр треугольника, заданного координатами своих вершин.
-        /// (Определить функцию для расчета длины отрезка по координатам его вершин.)
+        /// Задача 1. Написать программу определения площади пятиугольника с заданными координатами его вершин (x1,y1), (x2,y2), (x3,y3), (x4,y4), (x5,y5)
+        /// как сумму площадей трех треугольников. Площадь треугольника, определенную по координатам вершин, рассчитать с помощью функции.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            double x1 = Input("Введите x1: ");
-            double y1 = Input("Введите y1: ");
-            double x2 = Input("Введите x2: ");
-            double y2 = Input("Введите y2: ");
-            double x3 = Input("Введите x3: ");
-            double y3 = Input("Введите y3: ");
+            double x1 = Input("x1: ");
+            double y1 = Input("y1: ");
+            double x2 = Input("x2: ");
+            double y2 = Input("y2: ");
+            double x3 = Input("x3: ");
+            double y3 = Input("y3: ");
+            double x4 = Input("x4: ");
+            double y4 = Input("y4: ");
+            double x5 = Input("x5: ");
+            double y5 = Input("y5: ");
 
-            double perimeter = P(x1, y1, x2, y2, x3, y3);
-            Console.WriteLine($"Периметр треугольника: {perimeter}");
+            double area = P(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5);
+            Console.WriteLine($"Площадь пятиугольника: {area}");
             Console.ReadLine();
         }
     }
