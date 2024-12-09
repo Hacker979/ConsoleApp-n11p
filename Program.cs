8e44d8bc7b80eccc41dea1a2fcde62681c4832bb
jotaro4u <kothaker135@gmail.com>
2024-12-10 00:50:23 +0300

    ппуы

--- Program.cs
+++ Program.cs
@@ -8,49 +8,44 @@ namespace ConsoleApp_n11p
 {
     class Program
     {
-        static double Input(string msg)
+        static int MaxTwo(int a, int b)
         {
-            Console.Write(msg);
-            return double.Parse(Console.ReadLine());
+            return a > b ? a : b; // Возвращает большее из двух чисел
         }
 
-        static double Dist(double x1, double y1, double x2, double y2)
+        static int FindMax(int a, int b, int c, int d, int e, int f, int g, int h)
         {
-            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            int max1 = MaxTwo(a, b);
+            int max2 = MaxTwo(c, d);
+            int max3 = MaxTwo(e, f);
+            int max4 = MaxTwo(g, h);
+            int max = MaxTwo(max1, MaxTwo(max2, MaxTwo(max3, max4)));
+            return max;
         }
 
-        static double Area(double x1, double y1, double x2, double y2, double x3, double y3)
+        // Метод для ввода числа
+        static int Input(string prompt)
         {
-            return Math.Abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)) / 2;
-        }
-
-        static double P(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4, double x5, double y5)
-        {
-            double area1 = Area(x1, y1, x2, y2, x3, y3);
-            double area2 = Area(x1, y1, x3, y3, x4, y4);
-            double area3 = Area(x1, y1, x4, y4, x5, y5);
-            return area1 + area2 + area3;
+            Console.Write(prompt);
+            return int.Parse(Console.ReadLine());
         }
         /// <summary>
-        /// Задача 1. Написать программу определения площади пятиугольника с заданными координатами его вершин (x1,y1), (x2,y2), (x3,y3), (x4,y4), (x5,y5)
-        /// как сумму площадей трех треугольников. Площадь треугольника, определенную по координатам вершин, рассчитать с помощью функции.
+        /// Задача 2. Даны 8 различных чисел. Определить максимальное из них, используя функцию определения максимального из двух чисел.
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            double x1 = Input("x1: ");
-            double y1 = Input("y1: ");
-            double x2 = Input("x2: ");
-            double y2 = Input("y2: ");
-            double x3 = Input("x3: ");
-            double y3 = Input("y3: ");
-            double x4 = Input("x4: ");
-            double y4 = Input("y4: ");
-            double x5 = Input("x5: ");
-            double y5 = Input("y5: ");
+            int a = Input("Введите 1 число: ");
+            int b = Input("Введите 2 число: ");
+            int c = Input("Введите 3 число: ");
+            int d = Input("Введите 4 число: ");
+            int e = Input("Введите 5 число: ");
+            int f = Input("Введите 6 число: ");
+            int g = Input("Введите 7 число: ");
+            int h = Input("Введите 8 число: ");
 
-            double area = P(x1, y1, x2, y2, x3, y3, x4, y4, x5, y5);
-            Console.WriteLine($"Площадь пятиугольника: {area}");
+            int max = FindMax(a, b, c, d, e, f, g, h);
+            Console.WriteLine($"Максимальное число: {max}");
             Console.ReadLine();
         }
     }
